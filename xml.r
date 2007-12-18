91d0cf317106e78a031cf4ce0c0bab04cf582146
bigbear <bigbear@478a9d2e-6004-0410-ac81-b4d184805fe2>
2007-12-18 19:49:23 +0000

    Made it compatible for the functions
    git-svn-id: svn://ggobi.org/ggobi/packages/geozoo@5425 478a9d2e-6004-0410-ac81-b4d184805fe2

--- xml.r
+++ xml.r
@@ -2,7 +2,5 @@ source("/Users/barret/Sites/Geometric Website/geometric-data/cube/codes/writeXML
 
 write.xml <- function(object.function,filepath,title) {
 
-object.function
-
-f.writeXML(dat1= vert,filename= filepath,data.num=2,data.name= title,dat1.name="vertices",dat2=wires,dat2.source=wires[,1],dat2.destination=wires[,2],dat2.name="edges", default.color="5")
+f.writeXML(dat1= object.function$points,filename= filepath,data.num=2,data.name= title,dat1.name="vertices",dat2=object.function$edges,dat2.source=object.function$edges[,1],dat2.destination=object.function$edges[,2],dat2.name="edges", default.color="5")
 }
