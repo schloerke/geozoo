bd492e8c479f246b77cf591b8145fc9dc4280316
Barret Schloerke <schloerke@gmail.com>
2016-01-12 00:56:07 -0500

    only display messages if tourr is going to be run

--- R/print.R
+++ R/print.R
@@ -17,17 +17,18 @@
 print.geozoo <- function(x, ...){
   colnames(x$points) <- paste("V", seq_len(ncol(x$points)))
 
-  if (nrow(x$points) > 1000) {
-    message("Reduce points to improve performance\n")
-  }
-
-  if (!is.null(x$edges)) {
-    message("Edges will not be printed using 'tourr'\n")
-  }
 
   if ( (! requireNamespace("tourr")) || (! interactive()) ) {
     as.list(x)
   } else {
+    if (nrow(x$points) > 1000) {
+      message("Reduce points to improve performance\n")
+    }
+
+    if (!is.null(x$edges)) {
+      message("Edges will not be printed using 'tourr'\n")
+    }
+
     tourr::animate(
       x$points,
       tour_path = tourr::grand_tour(),
